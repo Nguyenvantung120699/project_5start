ff32a1ebd0e4b91c50b34293ba09294c3822821a
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-03-31 16:53:01 +0700

    tungupdate

--- resources/views/html/head.blade.php
+++ resources/views/html/head.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <head>
   <!-- Required meta tags -->
   <meta charset="utf-8" />
@@ -37,42 +36,3 @@
   <script src="{{asset("js/mail-script.js")}}"></script>
   <script src="{{asset("js/theme.js")}}"></script>
 </head>
-=======
-<!-- Required meta tags -->
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-<link rel="icon" href="img/favicon.png" type="image/png" />
-<title>Eiser @yield('title')</title>
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="{{asset("css/bootstrap.css")}}" />
-<link rel="stylesheet" href="{{asset("vendors/linericon/style.css")}}" />
-<link rel="stylesheet" href="{{asset("css/font-awesome.min.css")}}" />
-<link rel="stylesheet" href="{{asset("css/themify-icons.css")}}" />
-<link rel="stylesheet" href="{{asset("css/flaticon.css")}}" />
-<link rel="stylesheet" href="{{asset("vendors/owl-carousel/owl.carousel.min.css")}}" />
-<link rel="stylesheet" href="{{asset("vendors/lightbox/simpleLightbox.css")}}" />
-<link rel="stylesheet" href="{{asset("vendors/nice-select/css/nice-select.css")}}" />
-<link rel="stylesheet" href="{{asset("vendors/animate-css/animate.css")}}" />
-<link rel="stylesheet" href="{{asset("vendors/jquery-ui/jquery-ui.css")}}" />
-
-<!-- main css -->
-<link rel="stylesheet" href="{{asset("css/style.css")}}" />
-<link rel="stylesheet" href="{{asset("css/responsive.css")}}" />
-
-
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="{{asset("js/jquery-3.2.1.min.js")}}"></script>
-<script src="{{asset("js/popper.js")}}"></script>
-<script src="{{asset("js/bootstrap.min.js")}}"></script>
-<script src="{{asset("js/stellar.js")}}"></script>
-<script src="{{asset("vendors/lightbox/simpleLightbox.min.js")}}"></script>
-<script src="{{asset("vendors/nice-select/js/jquery.nice-select.min.js")}}"></script>
-<script src="{{asset("vendors/isotope/imagesloaded.pkgd.min.js")}}"></script>
-<script src="{{asset("vendors/isotope/isotope-min.js")}}"></script>
-<script src="{{asset("vendors/owl-carousel/owl.carousel.min.js")}}"></script>
-<script src="{{asset("js/jquery.ajaxchimp.min.js")}}"></script>
-<script src="{{asset("vendors/counter-up/jquery.waypoints.min.js")}}"></script>
-<script src="{{asset("vendors/counter-up/jquery.counterup.js")}}"></script>
-<script src="{{asset("js/mail-script.js")}}"></script>
-<script src="{{asset("js/theme.js")}}"></script>
->>>>>>> e52c75ce6844446d1a72567d8e0353697a104c75
