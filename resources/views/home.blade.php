5d382f9dd0edb241ecf99569e13d5e6997f02e41
nguyenthininh <45448845+nguyenthininh@users.noreply.github.com>
2020-04-01 15:17:21 +0700

    ninh up anh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,8 +72,8 @@
       <div class="row justify-content-center">
         <div class="col-lg-12">
           <div class="main_title">
-            <h2><span>Featured product</span></h2>
-            <p>Bring called seed first of third give itself now ment</p>
+            <h2><span>Category Listing</span></h2>
+            <p>Click on the catalog you want to see to know more about our products</p>
           </div>
         </div>
       </div>
@@ -89,9 +89,6 @@
                 </a>
                 <a href="#">
                   <i class="ti-heart"></i>
-                </a>
-                <a href="#">
-                  <i class="ti-shopping-cart"></i>
                 </a>
               </div>
             </div>
@@ -163,189 +160,12 @@
           </div>
         </div>
       </div>
-    </div>
-  </section>
-  <!--================ End Feature Product Area =================-->
-
-  <!--================ Offer Area =================-->
-  <section class="offer_area">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="offset-lg-4 col-lg-6 text-center">
-          <div class="offer_content">
-            <h3 class="text-uppercase mb-40">all men’s collection</h3>
-            <h2 class="text-uppercase">50% off</h2>
-            <a href="#" class="main_btn mb-20 mt-5">Discover Now</a>
-            <p>Limited Time Offer</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!--================ End Offer Area =================-->
-
-  <!--================ New Product Area =================-->
-  <section class="new_product_area section_gap_top section_gap_bottom_custom">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-lg-12">
-          <div class="main_title">
-            <h2><span>new products</span></h2>
-            <p>Bring called seed first of third give itself now ment</p>
-          </div>
-        </div>
-      </div>
-
+      
       <div class="row">
-        <div class="col-lg-6">
-          <div class="new_product">
-            <h5 class="text-uppercase">collection of 2019</h5>
-            <h3 class="text-uppercase">Men’s summer t-shirt</h3>
-            <div class="product-img">
-              <img class="img-fluid" src="img/product/new-product/new-product1.png" alt="" />
-            </div>
-            <h4>$120.70</h4>
-            <a href="#" class="main_btn">Add to cart</a>
-          </div>
-        </div>
-
-        <div class="col-lg-6 mt-5 mt-lg-0">
-          <div class="row">
-            <div class="col-lg-6 col-md-6">
-              <div class="single-product">
-                <div class="product-img">
-                  <img class="img-fluid w-100" src="img/product/new-product/n1.jpg" alt="" />
-                  <div class="p_icon">
-                    <a href="#">
-                      <i class="ti-eye"></i>
-                    </a>
-                    <a href="#">
-                      <i class="ti-heart"></i>
-                    </a>
-                    <a href="#">
-                      <i class="ti-shopping-cart"></i>
-                    </a>
-                  </div>
-                </div>
-                <div class="product-btm">
-                  <a href="#" class="d-block">
-                    <h4>Nike latest sneaker</h4>
-                  </a>
-                  <div class="mt-3">
-                    <span class="mr-4">$25.00</span>
-                    <del>$35.00</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-6 col-md-6">
-              <div class="single-product">
-                <div class="product-img">
-                  <img class="img-fluid w-100" src="img/product/new-product/n2.jpg" alt="" />
-                  <div class="p_icon">
-                    <a href="#">
-                      <i class="ti-eye"></i>
-                    </a>
-                    <a href="#">
-                      <i class="ti-heart"></i>
-                    </a>
-                    <a href="#">
-                      <i class="ti-shopping-cart"></i>
-                    </a>
-                  </div>
-                </div>
-                <div class="product-btm">
-                  <a href="#" class="d-block">
-                    <h4>Men’s denim jeans</h4>
-                  </a>
-                  <div class="mt-3">
-                    <span class="mr-4">$25.00</span>
-                    <del>$35.00</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-6 col-md-6">
-              <div class="single-product">
-                <div class="product-img">
-                  <img class="img-fluid w-100" src="img/product/new-product/n3.jpg" alt="" />
-                  <div class="p_icon">
-                    <a href="#">
-                      <i class="ti-eye"></i>
-                    </a>
-                    <a href="#">
-                      <i class="ti-heart"></i>
-                    </a>
-                    <a href="#">
-                      <i class="ti-shopping-cart"></i>
-                    </a>
-                  </div>
-                </div>
-                <div class="product-btm">
-                  <a href="#" class="d-block">
-                    <h4>quartz hand watch</h4>
-                  </a>
-                  <div class="mt-3">
-                    <span class="mr-4">$25.00</span>
-                    <del>$35.00</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-6 col-md-6">
-              <div class="single-product">
-                <div class="product-img">
-                  <img class="img-fluid w-100" src="img/product/new-product/n4.jpg" alt="" />
-                  <div class="p_icon">
-                    <a href="#">
-                      <i class="ti-eye"></i>
-                    </a>
-                    <a href="#">
-                      <i class="ti-heart"></i>
-                    </a>
-                    <a href="#">
-                      <i class="ti-shopping-cart"></i>
-                    </a>
-                  </div>
-                </div>
-                <div class="product-btm">
-                  <a href="#" class="d-block">
-                    <h4>adidas sport shoe</h4>
-                  </a>
-                  <div class="mt-3">
-                    <span class="mr-4">$25.00</span>
-                    <del>$35.00</del>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!--================ End New Product Area =================-->
-
-  <!--================ Inspired Product Area =================-->
-  <section class="inspired_product_area section_gap_bottom_custom">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-lg-12">
-          <div class="main_title">
-            <h2><span>Inspired products</span></h2>
-            <p>Bring called seed first of third give itself now ment</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-lg-3 col-md-6">
+        <div class="col-lg-4 col-md-6">
           <div class="single-product">
             <div class="product-img">
-              <img class="img-fluid w-100" src="img/product/inspired-product/i1.jpg" alt="" />
+              <img class="img-fluid w-100" src="img/product/feature-product/f-p-1.jpg" alt="" />
               <div class="p_icon">
                 <a href="#">
                   <i class="ti-eye"></i>
@@ -370,10 +190,10 @@
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6">
+        <div class="col-lg-4 col-md-6">
           <div class="single-product">
             <div class="product-img">
-              <img class="img-fluid w-100" src="img/product/inspired-product/i2.jpg" alt="" />
+              <img class="img-fluid w-100" src="img/product/feature-product/f-p-2.jpg" alt="" />
               <div class="p_icon">
                 <a href="#">
                   <i class="ti-eye"></i>
@@ -388,7 +208,7 @@
             </div>
             <div class="product-btm">
               <a href="#" class="d-block">
-                <h4>Latest men’s sneaker</h4>
+                <h4>Red women purses</h4>
               </a>
               <div class="mt-3">
                 <span class="mr-4">$25.00</span>
@@ -397,62 +217,7 @@
             </div>
           </div>
         </div>
-
-        <div class="col-lg-3 col-md-6">
-          <div class="single-product">
-            <div class="product-img">
-              <img class="img-fluid w-100" src="img/product/inspired-product/i3.jpg" alt="" />
-              <div class="p_icon">
-                <a href="#">
-                  <i class="ti-eye"></i>
-                </a>
-                <a href="#">
-                  <i class="ti-heart"></i>
-                </a>
-                <a href="#">
-                  <i class="ti-shopping-cart"></i>
-                </a>
-              </div>
-            </div>
-            <div class="product-btm">
-              <a href="#" class="d-block">
-                <h4>Latest men’s sneaker</h4>
-              </a>
-              <div class="mt-3">
-                <span class="mr-4">$25.00</span>
-                <del>$35.00</del>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6">
-          <div class="single-product">
-            <div class="product-img">
-              <img class="img-fluid w-100" src="img/product/inspired-product/i4.jpg" alt="" />
-              <div class="p_icon">
-                <a href="#">
-                  <i class="ti-eye"></i>
-                </a>
-                <a href="#">
-                  <i class="ti-heart"></i>
-                </a>
-                <a href="#">
-                  <i class="ti-shopping-cart"></i>
-                </a>
-              </div>
-            </div>
-            <div class="product-btm">
-              <a href="#" class="d-block">
-                <h4>Latest men’s sneaker</h4>
-              </a>
-              <div class="mt-3">
-                <span class="mr-4">$25.00</span>
-                <del>$35.00</del>
-              </div>
-            </div>
-          </div>
-        </div>
+<<<<<<< HEAD
 
         <div class="col-lg-3 col-md-6">
           <div class="single-product">
@@ -539,9 +304,13 @@
         </div>
 
         <div class="col-lg-3 col-md-6">
+=======
+        
+        <div class="col-lg-4 col-md-6">
+>>>>>>> 5a12e59d0e8985c1f754cc287faf814c4152b8ea
           <div class="single-product">
             <div class="product-img">
-              <img class="img-fluid w-100" src="img/product/inspired-product/i8.jpg" alt="" />
+              <img class="img-fluid w-100" src="img/product/feature-product/f-p-3.jpg" alt="" />
               <div class="p_icon">
                 <a href="#">
                   <i class="ti-eye"></i>
@@ -556,7 +325,7 @@
             </div>
             <div class="product-btm">
               <a href="#" class="d-block">
-                <h4>Latest men’s sneaker</h4>
+                <h4>Men stylist Smart Watch</h4>
               </a>
               <div class="mt-3">
                 <span class="mr-4">$25.00</span>
@@ -568,12 +337,13 @@
       </div>
     </div>
   </section>
-  <!--================ End Inspired Product Area =================-->
+  <!--================ End Feature Product Area =================-->
 
-  <!--================ Start Blog Area =================-->
-  <section class="blog-area section-gap">
+  <!--================ Offer Area =================-->
+  <section class="offer_area">
     <div class="container">
       <div class="row justify-content-center">
+<<<<<<< HEAD
         <div class="col-lg-12">
           <div class="main_title">
             <h2><span>latest blog</span></h2>
@@ -655,9 +425,22 @@
               </div>
               <a href="#" class="blog_btn">Learn More <span class="ml-2 ti-arrow-right"></span></a>
             </div>
+=======
+        <div class="offset-lg-4 col-lg-6 text-center">
+          <div class="offer_content">
+            <h3 class="text-uppercase mb-40">all men’s collection</h3>
+            <h2 class="text-uppercase">50% off</h2>
+            <a href="#" class="main_btn mb-20 mt-5">Discover Now</a>
+            <p>Limited Time Offer</p>
+>>>>>>> 5a12e59d0e8985c1f754cc287faf814c4152b8ea
           </div>
         </div>
       </div>
     </div>
   </section>
+<<<<<<< HEAD
   @endsection
+=======
+  <!--================ End Offer Area =================-->
+  @endsection
+>>>>>>> 5a12e59d0e8985c1f754cc287faf814c4152b8ea
