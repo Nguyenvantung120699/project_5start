91ab511cf5b1b11e07466b326f7b913970da8836
nguyenthininh <45448845+nguyenthininh@users.noreply.github.com>
2020-04-06 18:02:22 +0700

    ninh up email

---
+++ resources/views/emails/usercreated.blade.php
@@ -0,0 +1,49 @@
+<td class="esd-stripe" align="center" esd-custom-block-id="88658">
+    <table class="es-content-body" style="background-color: transparent;" width="600" cellspacing="0" cellpadding="0" align="center">
+        <tbody>
+        <tr>
+            <td class="esd-structure es-p10t es-p10b es-p10r es-p10l" esd-general-paddings-checked="false" align="left">
+                <!--[if mso]><table width="580" cellpadding="0" cellspacing="0"><tr><td width="369" valign="top"><![endif]-->
+                <table class="es-left" cellspacing="0" cellpadding="0" align="left">
+                    <tbody>
+                    <tr>
+                        <td class="es-m-p0r es-m-p20b esd-container-frame" width="369" valign="top" align="center">
+                            <table width="100%" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                <tr>
+                                    <td class="esd-block-text es-infoblock" align="left">
+                                        <p style="line-height: 150%;">Put your preheader text here</p>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <!--[if mso]></td><td width="20"></td><td width="191" valign="top"><![endif]-->
+                <table cellspacing="0" cellpadding="0" align="right">
+                    <tbody>
+                    <tr>
+                        <td class="esd-container-frame" width="191" align="left">
+                            <table width="100%" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                <tr>
+                                    <td esdev-links-color="#666666" align="right" class="esd-block-text es-infoblock">
+                                        <p style="line-height: 150%;"><a href="https://viewstripo.email" target="_blank" class="view">Not displaying correctly?</a></p>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <!--[if mso]></td></tr></table><![endif]-->
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</td>
+
+
