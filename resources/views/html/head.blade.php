89516013ee21593ffb75a6b91dfdc84623b4b466
Nguyễn Văn Tùng <49889365+Nguyenvantung120699@users.noreply.github.com>
2020-04-06 18:47:15 +0700

    tungUpdate_ajax

--- resources/views/html/head.blade.php
+++ resources/views/html/head.blade.php
@@ -19,33 +19,21 @@
   <link rel="stylesheet" href="{{asset("css/style.css")}}">
   <link rel="stylesheet" href="{{asset("css/responsive.css")}}" >
 
-  <script type="text/javascript" src="{{asset("js/jquery-3.4.1.js")}}"></script>
-  <!--===============================================================================================-->	
+  <!--<script type="text/javascript" src="{{asset("js/jquery-3.4.1.js")}}"></script>
 	<link rel="icon" type="image/png" href="{{ asset("login_v11/images/icons/favicon.ico")}}"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/vendor/bootstrap/css/bootstrap.min.css")}}">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/fonts/font-awesome-4.7.0/css/font-awesome.min.css")}}">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/fonts/Linearicons-Free-v1.0.0/icon-font.min.css")}}">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/vendor/animate/animate.css")}}">
-<!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/vendor/css-hamburgers/hamburgers.min.css")}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/vendor/select2/select2.min.css")}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/css/util.css")}}">
-	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/css/main.css")}}">
-<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset("login_v11/vendor/select2/select2.min.css")}}">-->
+  <link rel="stylesheet" type="text/css" href="{{ asset("login_v11/css/util.css")}}">
+  <link rel="stylesheet" type="text/css" href="{{ asset("login_v11/vendor/bootstrap/css/bootstrap.min.css")}}">
+  <link rel="stylesheet" type="text/css" href="{{ asset("login_v11/css/main.css")}}">
 
-<!-- <script src="{{ asset("login_v11/vendor/jquery/jquery-3.2.1.min.js")}}"></script> -->
-<!--===============================================================================================-->
-	<script src="{{ asset("login_v11/vendor/bootstrap/js/popper.js")}}"></script>
+  <!-- <script src="{{ asset("login_v11/vendor/jquery/jquery-3.2.1.min.js")}}"></script>
 	<script src="{{ asset("login_v11/vendor/bootstrap/js/bootstrap.min.js")}}"></script>
-<!--===============================================================================================-->
 	<script src="{{ asset("login_v11/vendor/select2/select2.min.js")}}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset("login_v11/js/main.js")}}"></script>
+  <script src="{{ asset("login_v11/js/main.js")}}"></script> 
+   -->
 
 </head>
